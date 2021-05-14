a3defa7951787f53ff7938111a9162981f126fc0
PolianaSouzaAnanias <poliana.souzaa@hotmail.com>
2021-05-13 22:36:07 -0300

    Adicionados comentarios nas linhas

--- CursoLogicaCapitulo02/src/troco/CalcularValorTroco.java
+++ CursoLogicaCapitulo02/src/troco/CalcularValorTroco.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 
 public class CalcularValorTroco {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in); //
 		
-		System.out.print("Digite o valor do produto: ");
-		Double valorProduto = scanner.nextDouble();
+		System.out.print("Digite o valor do produto: "); //Escrever mensagem no console.
+		Double valorProduto = scanner.nextDouble(); //A variável 'valorProduto' vai armazenar um valor do tipo numérico (decimal).
 		
-		System.out.print("Digite quantidade passada pelo cliente: ");
-		Double valorPassadoPeloCliente = scanner.nextDouble();
+		System.out.print("Digite quantidade passada pelo cliente: "); //Escrever mensagem no console.
+		Double valorPassadoPeloCliente = scanner.nextDouble(); //A variável 'valorPassadoPeloCliente' vai armazenar um valor do tipo numérico (decimal).
 		
-		Double resultado = valorPassadoPeloCliente - valorProduto;
+		Double resultado = valorPassadoPeloCliente - valorProduto; //A variável  'resoltado' realizará o calculo dos valores recebidos.
 		
-		System.out.println("Troco: " + resultado);
+		System.out.println("Troco: " + resultado); // Comando para mostrar o resultado no console.
 		
-		scanner.close();
+		scanner.close(); //Encerra o scanner.
 		}
 }
