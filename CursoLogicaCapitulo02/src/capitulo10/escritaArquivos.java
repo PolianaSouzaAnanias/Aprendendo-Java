d10008d3e26d535dc56a98bb78ad2b0b8b01b98a
PolianaSouzaAnanias <poliana.souzaa@hotmail.com>
2021-08-24 20:35:51 -0300

    Leitura e Esxrita de dados em arquivos.

---
+++ CursoLogicaCapitulo02/src/capitulo10/escritaArquivos.java
@@ -0,0 +1,36 @@
+package capitulo10;
+
+public class escritaArquivos {
+
+	public static void main(String[] args) {
+		import java.io.IOException;
+		import java.nio.file.Files;
+		import java.nio.file.Path;
+		import java.nio.file.Paths;
+		import java.util.ArrayList;
+		import java.util.Scanner;
+
+		public class EscritaArquivos {
+
+			public static void main(String[] args) throws IOException {
+				Scanner scanner = new Scanner(System.in);
+
+				ArrayList<String> linhas = new ArrayList<String>();
+
+				for (int i = 0; i < 5; i++) {
+					System.out.print("Jogador " + i + ": ");
+					String nome = scanner.nextLine();
+
+					linhas.add(nome);
+				}
+
+				Path arquivo = Paths.get("/tmp/aula/arquivo.txt");
+				Files.write(arquivo, linhas);
+
+				scanner.close();
+				System.out.println("Fim...");
+			}
+		}
+	}
+
+}
